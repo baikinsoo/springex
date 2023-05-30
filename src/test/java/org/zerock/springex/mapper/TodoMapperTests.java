77e794f67ef82a6eaab69f9a6ec58309e01bf988
baekinsoo <baikinsoo95@gmail.com>
2023-05-30 16:25:23 +0900

    스프링 WEB MVC (1) --- 23.05.30 16:30

--- src/test/java/org/zerock/springex/mapper/TodoMapperTests.java
+++ src/test/java/org/zerock/springex/mapper/TodoMapperTests.java
@@ -1,11 +1,16 @@
 package org.zerock.springex.mapper;
 
+import com.sun.tools.javac.comp.Todo;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.zerock.springex.domain.TodoVO;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Log4j2
 @ExtendWith(SpringExtension.class)
@@ -18,5 +23,30 @@ public class TodoMapperTests {
     @Test
     public void testGetTime() {
         log.info(todoMapper.getTime());
+    }
+
+    @Test
+    public void testInsert() {
+        TodoVO todoVO = TodoVO.builder()
+                .title("스프링 테스트")
+                .dueDate(LocalDate.of(2022,10,10))
+                .writer("user00")
+                .build();
+
+        todoMapper.insert(todoVO);
+    }
+
+    @Test
+    public void testSelectAll() {
+        List<TodoVO> volist = todoMapper.selectAll();
+
+        volist.forEach(vo -> log.info(vo));
+    }
+
+    @Test
+    public void testSelectOne() {
+        TodoVO todoVO = todoMapper.selectOne(3L);
+
+        log.info(todoVO);
     }
 }
